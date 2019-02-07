dcef5e25e51a1f166644ed19a039b590f9e658f8
DavyGCLima <davy.g.c.lima@gmail.com>
2019-02-07 11:38:27 -0200

    pipes async

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
@@ -16,19 +17,20 @@ registerLocaleData(localePt);
     CamelCasePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
-    {
+    /*{
       provide: LOCALE_ID,
       useValue: 'pt-BR'
-    }/*
+    }*/
     SettingsService,
     {
       provide: LOCALE_ID,
       deps: [SettingsService],
       useFactory: (settingsService) => settingsService.getLocale()
-    }*/
+    }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExemplosPipesComponent implements OnInit {
 
+
+  /*
+    NÃO FAZER ITERAÇÕES EM ARRAYS COM PIPES
+  */
+
+
   book: any = {
     title: 'Algum livro',
     rating: 4.54321,
@@ -14,13 +20,38 @@ export class ExemplosPipesComponent implements OnInit {
     price: 39.99,
     releaseDate: new Date(2018, 6, 6),
     url: 'https://www.amazon.com.br/Criando-apps-para-empresas-'
-    + 'Android-ebook/dp/B01IJQ0AU6/ref=sr_1_2?__mk_pt_BR=%C3%85M%'
-    + 'C3%85%C5%BD%C3%95%C3%91&keywords=android&qid=1549461208&sr=8-2'
+      + 'Android-ebook/dp/B01IJQ0AU6/ref=sr_1_2?__mk_pt_BR=%C3%85M%'
+      + 'C3%85%C5%BD%C3%95%C3%91&keywords=android&qid=1549461208&sr=8-2'
   };
+
+  books: string[] = ['Java', 'Angular 2'];
+
+  filter: string;
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  addBook(value) {
+    this.books.push(value);
+  }
+
+  getBooks() {
+    if (this.books.length === 0 || this.filter === undefined || this.filter.trim() === '') {
+      return this.books;
+    }
+
+    return this.books.filter((v) => {
+      if (v.toLowerCase().indexOf(this.filter.toLowerCase()) >= 0) {
+        return true;
+      }
+      return false;
+    });
+  }
+
+  asyncValue = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor assíncrono'), 2000)
+  });
 
 }
